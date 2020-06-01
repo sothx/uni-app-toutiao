17645e736fd95c0ef0e66a01fe4e928cae3f61ed
sothx <730317940@qq.com>
2020-06-01 18:41:29 +0800

    初始化

---
+++ components/home/homeHeader.vue
@@ -0,0 +1,13 @@
+<template>
+	<view class="home-hader">
+		<view class="serach-bar">
+			<input type="text" class="search-input" placeholder="学习编程找米修在线">
+		</view>
+	</view>
+</template>
+
+<script>
+</script>
+
+<style lang="scss">
+</style>
